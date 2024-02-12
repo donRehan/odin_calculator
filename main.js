c89ed8c2bfe47ac95b45ed961de72b6849e4d768
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-02-12 15:36:12 +0200

    Add basic page layout

--- main.js
+++ main.js
@@ -3,9 +3,27 @@ let subtract = (a, b) => a - b;
 let divide = (a, b) => a / b;
 let multiply = (a, b) => a * b;
 
+let first_number = 0;
+let second_number = 0;
+let operand = 0;
+
+function operate(first_number, second_number, operand) {
+	switch(operand) {
+		case '+':
+			return add(first_number, second_number);
+		case '-':
+			return subtract(first_number, second_number);
+		case '/':
+			return divide(first_number, second_number);
+		case '*':
+			return multiply(first_number, second_number);
+	}
+}
+
 module.exports = {
 	add,
 	subtract,
 	divide,
-	multiply
+	multiply,
+	operate
 }
--- test.mjs
+++ test.mjs
@@ -1,38 +1,39 @@
-import {add, subtract, divide, multiply} from './main.js';
+import {operate, add, subtract, divide, multiply} from './main.js';
 
-//test add
+////test add
 let result = add(1, 2);
 let expected = 3;
+
+//test operate
+result = operate(1, 2, '+');
+expected = 3;
 if (result !== expected) {
 	throw new Error(`${result} is not equal to ${expected} ⚠️`);
 }
 else {
-	console.log('add test passed ✓');
+	console.log('operate addition test passed ✓');
 }
-//test subtract
-result = subtract(1, 2);
+result = operate(1, 2, '-');
 expected = -1;
 if (result !== expected) {
 	throw new Error(`${result} is not equal to ${expected} ⚠️`);
 }
 else {
-	console.log('subtract test passed ✓');
+	console.log('operate subtraction test passed ✓');
 }
-//test divide
-result = divide(1, 2);
+result = operate(1, 2, '/');
 expected = 0.5;
 if (result !== expected) {
 	throw new Error(`${result} is not equal to ${expected} ⚠️`);
 }
 else {
-	console.log('divide test passed ✓');
+	console.log('operate division test passed ✓');
 }
-//test multiply
-result = multiply(1, 2);
+result = operate(1, 2, '*');
 expected = 2;
 if (result !== expected) {
 	throw new Error(`${result} is not equal to ${expected} ⚠️`);
 }
 else {
-	console.log('multiply test passed ✓');
+	console.log('operate multiplication test passed ✓');
 }
