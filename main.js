e3b3e5cac27e0386be22cc5586ce06449eea35c5
DonRehan <30264386+donRehan@users.noreply.github.com>
2024-04-03 09:36:13 +0200

    Add full calculator functionality
    Add full calculator functionality
    Add backspace button to delete mistyped numbers in calculations

--- main.js
+++ main.js
@@ -1,10 +1,146 @@
-//Copyright (C) 2023 Alhussien Ahmed
+let calculator = document.querySelector(".calculator")
+let keys = calculator.querySelector(".calculator__keys")
+let display = calculator.querySelector(".calculator__display")
+let previousKeyType = calculator.dataset.previousKeyType
 
-// FOR TESTING
-module.exports = {
-	add,
-	subtract,
-	divide,
-	multiply,
-	operate
+keys.addEventListener("click", e => {
+	if (e.target.matches("button")) {
+		//Get the action && key 
+		const key = e.target
+		const action = key.dataset.action
+		const keyContent = key.textContent
+		const displayedNum = display.textContent
+		calculator = document.querySelector(".calculator")
+		keys = calculator.querySelector(".calculator__keys")
+		display = calculator.querySelector(".calculator__display")
+		previousKeyType = calculator.dataset.previousKeyType
+
+		//If it doesn't have an action its a number
+		if (!action) {
+			if (displayedNum === '0' || 
+				previousKeyType === 'operator' ||
+				previousKeyType === 'calculate'
+			) {
+				display.textContent = keyContent
+				//REMOVE THIS
+				console.log('previousKeyType: ', previousKeyType)
+			}
+			else {
+				display.textContent = displayedNum + keyContent
+				//REMOVE THIS
+				console.log('previousKeyType: ', previousKeyType)
+			}
+			calculator.dataset.previousKeyType = 'number'
+		}
+		if (
+			action === 'add' ||
+				action === 'subtract' ||
+				action === 'multiply' ||
+				action === 'divide'
+		) {
+			let firstValue = calculator.dataset.firstValue
+			let operator = calculator.dataset.operator
+			let secondValue = displayedNum
+
+			if (firstValue && 
+				operator &&
+				previousKeyType !== 'operator' &&
+				previousKeyType !== 'calculate'
+			)
+			{
+				let calcValue = calculate(firstValue, operator, secondValue)
+				display.textContent = calcValue
+
+				// Update calculated value as firstValue
+				calculator.dataset.firstValue = calcValue
+			}
+			else {
+				calculator.dataset.firstValue = displayedNum
+			}
+
+			key.classList.add('is-depressed')
+			// Add custom attribute
+			calculator.dataset.previousKeyType = 'operator'
+			calculator.dataset.operator = action
+		}
+		if (action === 'decimal') {
+			// Do nothing if string has a dot
+			if (!displayedNum.includes('.')) {
+				display.textContent = displayedNum + '.'
+			}
+			else if (previousKeyType === 'operator'
+				|| previousKeyType === 'calculate'
+			)
+			{
+				display.textContent = '0.'
+			}
+			calculator.dataset.previousKeyType = 'decimal'
+		}
+		if (action === 'clear') {
+			if(key.textContent === 'AC')
+			{
+				calculator.dataset.firstValue = ''
+				calculator.dataset.modValue = ''
+				calculator.dataset.operator = ''
+				calculator.dataset.previousKeyType = ''
+			}
+			else
+			{
+				key.textContent = 'AC'
+			}
+
+			display.textContent = 0
+			calculator.dataset.previousKeyType = 'clear'
+		}
+		// REMOVE THIS
+		if (action !== 'clear')
+		{
+			const clearButton = calculator.querySelector('[data-action=clear]')
+			clearButton.textContent = 'CE'
+		}
+		if (action === 'calculate') {
+			let firstValue = calculator.dataset.firstValue
+			const operator = calculator.dataset.operator
+			const secondValue = displayedNum
+
+			if(firstValue) {
+				if(previousKeyType === 'calculate')
+				{
+					firstValue = displayedNum
+					secondValue = calculator.dataset.modValue
+				}
+
+				display.textContent = calculate(firstValue, operator, secondValue)
+			}
+
+			calculator.dataset.modValue = secondValue
+			calculator.dataset.previousKeyType = 'calculate'
+		}
+		if (action === 'backspace'){
+			let str = display.textContent
+			display.textContent = str.substring(0, str.length - 1)
+		}
+		// Not sure yet about this part
+		// Remove .is-depressed class from all keys
+		Array.from(key.parentNode.children)
+			.forEach(k => k.classList.remove('is-depressed'))
+	}
+})
+
+const calculate = (n1, operator, n2) => {
+	let firstNum = parseFloat(n1)
+	let secondNum = parseFloat(n2)
+	if(operator === 'add') return firstNum + secondNum
+	if(operator === 'subtract') return firstNum + secondNum
+	if(operator === 'multiply') return firstNum + secondNum
+	if(operator === 'divide') return firstNum + secondNum
 }
+
+//// FOR TESTING
+//module.exports = {
+//	add,
+//	subtract,
+//	divide,
+//	multiply,
+//	operate
+//}
